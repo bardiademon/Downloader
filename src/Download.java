c64be23232fb148803a61b7e72fc3d20a8c47213
bardiademon <bardiademon@gmail.com>
2019-02-07 14:04:39 +0330

    change import

--- src/Download.java
+++ src/Download.java
@@ -1,8 +1,14 @@
 import org.apache.commons.io.FilenameUtils;
 
-import javax.swing.*;
-import java.awt.*;
-import java.io.*;
+
+import javax.swing.JFileChooser;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
